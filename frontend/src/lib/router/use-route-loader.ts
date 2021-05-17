f1405b75f742d5fdade6f8df39d4ca94061f093e
Lionel Seguin <lionel.seguin@contentsquare.com>
2021-05-17 19:08:21 +0200

    chore: ignore previous selected route

--- frontend/src/lib/router/use-route-loader.ts
+++ frontend/src/lib/router/use-route-loader.ts
@@ -7,17 +7,36 @@ export interface Route {
   Component: any;
 }
 
+function creatAbortable() {
+  let previous = null;
+  return {
+    new: () => {
+      let aborted = false;
+      previous?.abort();
+      previous = { abort: () => aborted = true };
+      return {
+        get aborted() {
+          return aborted;
+        }
+      };
+    },
+  };
+}
+
 export function useRouteLoader() {
   const loading = ref(false);
   const route = ref(null as Route);
+  const abortable = creatAbortable();
 
   async function load(def: RouteDef, params: any) {
+    const scope = abortable.new();
     try {
       loading.value = true;
       const [props, Component] = await Promise.all([
         def.paramsToProps ? def.paramsToProps(params) : Promise.resolve(params),
         def.loader(),
       ]);
+      if (scope.aborted) return;
       route.value = {
         name: def.name,
         props,
