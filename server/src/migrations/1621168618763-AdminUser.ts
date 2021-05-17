89c3899b0ea57363c2a0613e50781735493e8a5b
Lionel Seguin <lionelseguin@FR2404LSE.local>
2021-05-17 09:47:52 +0200

    chore: update admin user migration

--- server/src/migrations/1621168618763-AdminUser.ts
+++ server/src/migrations/1621168618763-AdminUser.ts
@@ -4,18 +4,20 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class AdminUser1621168618763 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Setup default User
         const admin = new User();
         admin.username = 'admin';
         admin.salt = User.generateSalt();
         admin.password = User.hashPassword(admin.salt, process.env.ADMIN_PASSWORD || 'admin');
         admin.email = process.env.ADMIN_EMAIL || 'admin@support.com';
+        admin.roles = ['admin'];
         
         await queryRunner.query(`INSERT INTO "user" ("username", "password", "salt", "email", "roles") VALUES ($1, $2, $3, $4, $5)`, [
             admin.username,
             admin.password,
             admin.salt,
             admin.email,
-            ['admin'],
+            admin.roles,
         ]);
     }
 
